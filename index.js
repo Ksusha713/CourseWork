2509ce5d22608a5f3bc14fcf68c4c75c6aab9228
KseniaShustikova <73019085+KseniaShustikova@users.noreply.github.com>
2025-05-16 20:34:50 +0200

    finish login and signup

--- index.js
+++ index.js
@@ -31,39 +31,74 @@ app.get("/products/:id", async (req, res) => {
   res.render(`${__dirname}/views/item.ejs`, { product:results[0] });
 });
 
+
+//LOGIN
 app.get("/login", (req, res) =>{
   res.render(`${__dirname}/views/login.ejs`)
 });
 
-app.post("/login", (req, res) => {
-  console.log(req.body)
-  const {
-    username, password 
-  } = req.body
-  const userFound = users.find(user => user.username == username && user.password == password)
-  if (userFound) {
-    res.send("All good")
+app.post("/login", async (req, res) => {
+  const {email, password} = req.body; 
+  if (!email || !password) {
+    return res.render("login", { error: "All fields are required" });
   }
-  else res.send("All bad")
+
+  const [results] = await connection.query(
+    `SELECT * FROM Users WHERE email = ? LIMIT 1`,
+    [email],
+  );
+  if (results.length === 0) {
+    return res.render("login", { error: "No account found with this email" });
+  }
+  const user = results[0];
+  const passwordMatch = await bcrypt.compare(password, user.password);
+  if (!passwordMatch) {
+    return res.render("login", { error: "Incorrect password" });
+  }
+  res.redirect(`/account/${user.UserID}`);
 });
 
+
+//SIGN UP
 app.get("/signup", (req, res) => {
   res.render(`${__dirname}/views/signup.ejs`)
 });
 
 app.post("/signup", async (req, res) => {
-  const {username, password, email} = req.body; //geting data from the form
-  if (!username || !password || !email) {
+  const {username, password, confirmpassword, email} = req.body; 
+  if (!username || !password || !confirmpassword || !email) {
     return res.render("signup", { error: "All fields are required" });
   }
   if (password.length < 8) {
     return res.render("signup", { error: "Password too short" });
   }
+  if (password !== confirmpassword) {
+    return res.render("signup", { error: "Passwords don't match"})
+  }
+
+  const [usersName] = await connection.query(
+    `SELECT * FROM Users WHERE username = ?`,
+    [username]
+  );
+  if (usersName.length > 0) {
+    return res.render("signup", {
+      error: "Username is taken"
+    })
+  }
+
+  const [usersEmail] = await connection.query(
+    `SELECT * FROM Users WHERE email = ?`,
+    [email]
+  );
+  if (usersEmail.length > 0) {
+    return res.redirect(`/login?error=email exists&email=${encodeURIComponent(email)}`);
+  }
+
   const hashedPassword = await bcrypt.hash(password, 10);
   const [result] = await connection.query(
     `INSERT INTO Users (username, password, email) VALUES (?, ?, ?)`, 
     [username, hashedPassword, email]
-  ); //question marks are placeholders to be filled later (sql injection protection), values to replace questions
+  );
   const newUserId = result.insertId;
   res.redirect(`/account/${newUserId}`);
 });
