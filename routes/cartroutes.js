bc7644b3f1d56c3fd1c3c99fbb6312c0a694f70a
Alaa Atarban <toalaa9@gmail.com>
2025-05-23 22:36:56 +0200

    cartroute

--- routes/cartroutes.js
+++ routes/cartroutes.js
@@ -10,24 +10,50 @@ let connection = await mysql.createConnection({
     database: "webshop",
 });
 
-router.post('/add-to-cart', (req, res) => {
+router.post('/add-to-cart', async (req, res) => {
     let { id, quantity } = req.body;
     if (!id || !quantity) {
         res.json({ status: 400, message: "Body is not complete" })
     }
-    let cart = req.cookies.cart;
-    if (!cart) {
-        cart = []
-    }
-    const product = cart.find(p => p.id == id);
-    if (product) {
-        quantity = parseInt(quantity)
-        product.quantity += quantity;
+	const user = req.cookies.User;
+	const [cart] = await connection.query(
+		`SELECT * FROM Cart WHERE UserID = ?`,
+		[user]
+	);
+	console.log()
+	let CartID;
+	let product;
+	if (!cart.length ) {
+        const [newCart] = await connection.query(`INSERT INTO Cart (userID) VALUES (?)`, 
+		[user]
+		);
+		console.log('newly createdn casdrysdf',newCart)
+		CartID = newCart.insertId
     } else {
+		CartID = cart[0].CartID;
+	}
+	const products = await connection.query(
+		`SELECT * FROM Products Where ProductID = ? AND CartID = ?`,
+		[id, CartID]
+	)
+    if (products.length > 0) {
         quantity = parseInt(quantity)
-        cart.push({ id, quantity })
-    }
-    res.cookie("cart", cart);
+        await connection.query(
+			`UPDATE CartItems
+			SET Quantity = ?
+			WHERE ProductID = ?`,
+			[quantity, id]
+		)
+    } else {
+        products = await connection.query(
+			`INSERT INTO CartItems (ProductID, Quantity, CartID) VALUES (?, ?, ?)`,
+		[id, quantity, CartID]	
+		)
+    } 
+	// const [result] = await connection.query(
+	// 	`INSERT INTO CartItems (ProductID, Quantity, CartID) VALUES (?, ?, ?)`, 
+	// 	[id, quantity, CartID]
+  	// );
     res.json({ status: 200, message: "Added" })
 });
 
@@ -55,3 +81,7 @@ router.get('/', async (req, res) => {
 });
 
 export default router
+
+// user from cookie
+// getcart from db using usersId
+//if not exist, create by inserting
