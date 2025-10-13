c8e9d472418d229683d08658158f75ecb21849bf
Ksusha713 <Kseniia.Shustikova@cs.khpi.edu.ua>
2025-10-13 19:16:25 +0200

    removed port from host

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(express.static('public'))
 app.use(jsonParser);
 
 let connection = await mysql.createConnection({
-	host: "ksusha-shop-ppqwm9:3306",
+	host: "ksusha-shop-ppqwm9",
 	user: "Ksusha",
 	password: "200ksenia200",
 	database: "webshop",
